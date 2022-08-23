e27bff445fcfae37b0206b2dbcaa4e8f77d7540d
Ali Sharabiani <sharabiani.a@gmail.com>
2022-08-23 03:37:43 -0400

    top 3

--- frontend/src/components/leaderboard.jsx
+++ frontend/src/components/leaderboard.jsx
@@ -3,12 +3,18 @@ import { TableContainer, Table, TableRow, TableCell, TableBody, Paper, Skeleton 
 export function Leaderboard({ students, loading }) {
 
   return (
-    <TableContainer component={Paper} sx={{ width: "300px"}}>
+    <TableContainer component={Paper} sx={{ width: "350px" }}>
       <Table aria-label='leaderboard table'>
         <TableBody>
           {
             students.map((x, index) => (
               <TableRow hover key={'ldr-row' + index}>
+                <TableCell>{
+                  index === 0 ? '1️st 🏆 🥇' :
+                    index === 1 ? '2nd 🥈' :
+                      index === 2 ? '3rd 🥉' :
+                        index + 1
+                }</TableCell>
                 <TableCell>{x.name}</TableCell>
                 <TableCell>{x['sum(point_types.point_value)']}</TableCell>
               </TableRow>))
