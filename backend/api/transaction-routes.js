296127647c6da28c7804d9cc6eb5b8848a0443f2
Ali Sharabiani <sharabiani.a@gmail.com>
2022-08-23 22:10:09 -0400

    transaction routes error handling

--- backend/api/transaction-routes.js
+++ backend/api/transaction-routes.js
@@ -2,26 +2,38 @@ const router = require('express').Router();
 const { addTransaction, getTransactions, getTransactionHistory } = require('../services/db-service');
 
 router.get('/', async (req, res) => {
-  const result = await getTransactions();
-  res.send(result);
+  try {
+    const result = await getTransactions();
+    res.send(result);
+  } catch (err) {
+    res.status.apply(500).send(err);
+  }
 });
 
 router.post('/', async (req, res) => {
   const { sId, pId, note } = req.body;
-  
+
   if (!sId) res.send(400).json('\'sId\' is required.');
   if (!pId) res.send(400).json('\'pId\' is required.');
-  
-  const result = await addTransaction(sId, pId, note);  
-  res.send(result); 
+  try {
+    const result = await addTransaction(sId, pId, note);
+    res.send(result);
+  } catch (err) {
+    res.status.apply(500).send(err);
+  }
 });
 
 router.get('/history/:cohortId', async (req, res) => {
   let { page, pageSize } = req.query;
   if (!page) page = 0;
   if (!pageSize) pageSize = 20;
-  const result = await getTransactionHistory(page, pageSize);
-  res.send(result);
+  try {
+    const result = await getTransactionHistory(page, pageSize);
+    res.send(result);
+  }
+  catch (err) {
+    res.status.apply(500).send(err);
+  }
 });
 
 
